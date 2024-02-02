60ea77fe811d7b03a1e8278783b6ba191308beed
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2024-02-02 16:24:39 +0000

    ANW part 3 complete

--- Amazing ninjas world/Assets/Scripts/CameraFollow.cs
+++ Amazing ninjas world/Assets/Scripts/CameraFollow.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public GameManager gameManager;
     public Transform target;
     public float smoothingTime = .2f;
     private Vector3 _velocity;
     public Vector3 CameraOffset;
+
+    public float deathHeight = -2;
+
+    private bool _following;
+    private float _cameraHeight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _following = true;
+        _cameraHeight = transform.position.y - 
+            Camera.main.ViewportToWorldPoint(Vector3.zero).y;
+
+        ResetView();
+    }
+
+    void Update()
+    {
+        _following = target.position.y > deathHeight;
+
+        if (target.gameObject.activeSelf && target.position.y <=
+            deathHeight - _cameraHeight)
+        {
+            gameManager.KillPlayer();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (!_following) return;
         Vector3 targetPos = new Vector3(target.position.x,target.position.y,target.position.z) + CameraOffset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPos,
         ref _velocity, smoothingTime);
+    }
+
+    public void ResetView()
+    {
+        transform.position = new Vector3(target.position.x,
+            target.position.y, transform.position.z) + CameraOffset;
+        _velocity = Vector3.zero;
     }
 }
--- Amazing ninjas world/Assets/Scripts/PlayerController.cs
+++ Amazing ninjas world/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameManager gameManager;
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
 
@@ -12,28 +13,50 @@ public class PlayerController : MonoBehaviour
     public LayerMask WhatIsGround;
 
     private bool _isGround;
+    private bool _enabled;
     private Rigidbody2D _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _enabled = true;
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
+        if (!_enabled) return;
         float movement = moveSpeed * Input.GetAxisRaw("Horizontal");
         _rigidbody.position += movement * Time.deltaTime * Vector2.right;
     }
 
     void Update()
     {
+        if (!_enabled) return;
         _isGround = Physics2D.Raycast(transform.position,Vector2.down,
         groundDistanceThreshold,WhatIsGround);
         
         if(_isGround && Input.GetButtonDown("Jump"))
         {
             _rigidbody.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    public void Enable()
+    {
+        _enabled = true;
+    }
+    public void Disable()
+    {
+        _enabled = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Hello");
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            gameManager.KillPlayer();
         }
     }
 }
